ac6206e66d791bc668e9fb678922ab797558248d
햇봇 <76088203+bighatbot@users.noreply.github.com>
2023-07-20 00:08:41 +0900

    Update add-nbo.cpp

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -2,15 +2,15 @@
 #include <stdint.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc) {
     FILE *f_1;
     FILE *f_2;
     uint32_t a;
     uint32_t b;
     uint32_t plus;
 
-    f_1 = fopen(argv[1], "rb");
-    f_2 = fopen(argv[2], "rb");
+    f_1 = fopen("a.bin", "rb");
+    f_2 = fopen("b.bin", "rb");
 
     fread(&a, sizeof(a), 1, f_1);
     fread(&b, sizeof(b), 1, f_2);
